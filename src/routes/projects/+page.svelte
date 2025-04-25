0d7dbcc23c1d21e23d714f522f05c2b9bd41f16b
Marissa Cui <marissacui@gmail.com>
2025-04-25 14:31:04 -0400

    updating url paths for images

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -7,27 +7,27 @@
         <ul class="projects">
             <li>
                 <article>
-                    <a href="/projects/a2" target="_blank">
+                    <a href="./a2" target="_blank">
                         <h3>A2: Exploratory Data Analysis</h3>
-                        <img src="../images/projects/preview_a2.png" alt="" />
+                        <img src="./images/projects/preview_a2.png" alt="" />
                     </a>
                     <p>Initial exploration of data under the subtheme of "Speculation".</p>
                 </article>
             </li>
             <li>                
                 <article>
-                    <a href="/projects/a3" target="_blank">
+                    <a href="./a3" target="_blank">
                         <h3>A3: Visualization Critique & Redesign</h3>
-                        <img src="../images/projects/preview_a3.png" alt="" />
+                        <img src="./images/projects/preview_a3.png" alt="" />
                     </a>
                     <p>A critique and redesign exercise for a New York Times Covid-19 graph.</p>
                 </article>
             </li>
             <li>
                 <article>
-                    <a href="/projects/a4" target="_blank">
+                    <a href="./a4" target="_blank">
                         <h3>A4: Persuasive or Deceptive Visualization</h3>
-                        <img src="../images/projects/preview_a4.png" alt="" />
+                        <img src="./images/projects/preview_a4.png" alt="" />
                     </a>
                     <p>An exercise to explore how the same set of data can be visualized to be for or against a certain claim.</p>
                 </article>
